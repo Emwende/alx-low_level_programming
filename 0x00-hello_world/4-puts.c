02e6e3b83c6af567d38c63b5ebd24a64060f46d1
Emwende <emwende89@gmail.com>
2022-06-30 16:08:43 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-
-* This program prints "programming is like building a multilingual puzzle"
-* return: 0 always (success)/
 int main(void)
 {
 	puts("\programming is like building a multilingual puzzle");
