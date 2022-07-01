ba7ed303c28b8de10a11fb13b839e6d5f8d257f0
Emwende <emwende89@gmail.com>
2022-07-01 14:24:09 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,24 +3,31 @@
 #include <stdlib.h>
 /**
  * main - prints all single digit numbers of base 10 starting from 0.
- * Description: Get a random number and check its last digit, compare it with 5
+ * and print whether the number stored in the variable n.
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	int n;
-	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last = n % 10;
+	printf(" last digit of %d is ", n)
+	if (n > 5)
+	{
+		printf("greater than 5");
+	}
 
-	if (last > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last);
-	else if (last == 0)
-		printf("Last digit of %d is %d and is 0\n", n, last);
-	else if (last < 6)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	if (n == 0)
+	{
+		printf(" 0 ");
+	}
+
+	if (n < 6 && n != 0)
+	{
+		printf("less than 6 and not 0 ");
+	}
+	printf("\n");
 
 	return (0);
 }
