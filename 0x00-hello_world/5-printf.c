7d995576b25577207d29ed4927ea5176e9c4637c
Emwende <emwende89@gmail.com>
2022-06-30 17:54:11 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-*main-print the string in the printf function
+*main- print the string in the printf function
 *
-* Description:using the main function
+* Description: using the main function
 * this program prints "with proper grammar, but the outcome is a piece of art
-* return: 0
+* return: Always 0 (success)
 */
 int main(void)
 {
