74b84a174984f0b65a42998df0b85d96ff8c05ff
Emwende <emwende89@gmail.com>
2022-07-01 16:09:12 -0700

    all alphaabets excep q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main-main prints the alphabet in lowercase
+*Description:followed by a new line
+*except q and e.
+*Return :Always 0 (success)
+*/
+int main(void)
+{
+	char ch = ' a ';
+
+	while (ch <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
