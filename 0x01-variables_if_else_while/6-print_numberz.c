a2c5b65e11744b52d99d57a3d2e73981020b3936
Emwende <emwende89@gmail.com>
2022-07-02 12:56:36 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main-prints all single digit numbers
+*of base 10 starting from 0, 
+*followed by a new line.
+*Return:Always 0 (success)
+*/
+int main(void)
+{
+	int c = 0;
+	
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+		putchar('\n');
+		return (0);	
+{
