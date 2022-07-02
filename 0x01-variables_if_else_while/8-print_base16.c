28c9c9d59d80f042b4def677c37620fb62654758
Emwende <emwende89@gmail.com>
2022-07-02 13:41:30 -0700

    print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main -all the numbers of base 16
+* in lowercase,
+*followed by a new line.
+*Return:Always 0 (success)
+*/
+int main(void)
+{
+	int i, y;
+	 for (i = 0; i < 10; i++)
+	 {
+		 putchar (i + '0');
+	 }
+	  for (y = 'a'; y <= 'f'; y++)
+	  {
+		      putchar (y);
+	  }
+	    putchar('\n');
+	     return (0);
+}
