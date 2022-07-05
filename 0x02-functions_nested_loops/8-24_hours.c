192514d085ca58020577dc114ef03868f8d25b87
Emwende <emwende89@gmail.com>
2022-07-05 16:52:55 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,36 +9,22 @@
 void jack_bauer(void)
 
 {
-int i, j
+int a;
+int b;
 
-for (i = 0; i < 24; i++)
+for (a = 0; a <= 23; a++)
 {
-for (j = 0; j < 60; j++)
+for (b = 0; b <= 59; b++;
 {
-if (i < 10)
-{
-_putchar ('0');
-_putchar (i + '0');
-}
+_putchar (a / 10 + '0');
+_putchar (a % 10 + '0');
+_putchar (':');
+_putchar (b / 10 + '0');
+_putchar (b % 10 + '0');
+_putchar ('\n');
 
-else if (i >= 10)
-{
-_putchar ((i / 10 + '0');
-_putchar ((i % 10 + '0');
 }
 
-if (j < 10)
-{
-_putchar (' : ');
-_putchar ('0');
-_putchar (j);
+
 }
 
-else if  (j > 10)
-{
-_putchar (' : ');
-_putchar (j / 10 + '0');
-_putchar (j % 10 + '0');
-}
-}
-}
