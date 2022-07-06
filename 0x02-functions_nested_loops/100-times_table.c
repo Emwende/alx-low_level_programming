8c4ccfa7a3e05593658e8b4248d2933e6cf062fe
Emwende <emwende89@gmail.com>
2022-07-06 12:09:57 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,6 +36,7 @@ _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
+
 else if (k >= 100)
 {
 _putchar(',');
@@ -45,7 +46,7 @@ _putchar(((k / 10) % 10) + '0');
 _putchar((k % 10) + '0');
 }
 }
-}
 _putchar('\n')
+}
 }
 }
