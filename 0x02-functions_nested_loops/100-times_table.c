20713fbdef055e552748330d82457f8dc2051a30
Emwende <emwende89@gmail.com>
2022-07-05 17:59:37 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,7 +18,8 @@ k = j * i;
 if (j == 0)
 _putchar(k + '0');
 }
-else if (k < 10 && j _putchar(',');
+if (k < 10 && j _putchar(',')
+
 {
 _putchar(',');
 _putchar(' ');
@@ -44,7 +45,7 @@ _putchar(((k / 10) % 10) + '0');
 _putchar((k % 10) + '0');
 }
 }
-_putchar('\n');
 }
+_putchar('\n')
 }
 }
