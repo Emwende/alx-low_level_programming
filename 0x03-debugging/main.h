15630ed9aa00161bd5804fbdfceed2cbcea8d641
Emwende <emwende89@gmail.com>
2022-07-07 13:44:57 -0700

    1-main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,7 +9,7 @@
 
 
 void positive_or_negative(int i);
-int largest_number(int, int, int);
+int largest_number(int a, int b, int c);
 void print_remaining_days(int, int, int);
 int convert_day(int month, int day);
 
