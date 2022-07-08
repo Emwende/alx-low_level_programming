e015971f110080eb9df70432185ad84b03ada0ab
Emwende <emwende89@gmail.com>
2022-07-08 14:17:06 -0700

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
-*print_most_numbers - prints
-*
-*Return: void
+*print_most_numbers - prints most numbers
 */
-
 void print_most_numbers(void)
 {
-	char c;
+	int x;
 
-	for (c = '0'; c <= '9'; c++)
+	for (x = 0; x < 10; x++)
 	{
-	if (!(c == '2' || c == '4'))
-	_putchar(c);
+		if (x != 2 && x != 4)
+			_putchar(x + '0');
 	}
 	_putchar('\n');
 }
