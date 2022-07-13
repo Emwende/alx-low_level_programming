907f664bb4902c3b2f76cf0afbe7335ae5e8fb24
Emwende <emwende89@gmail.com>
2022-07-13 12:29:29 -0700

    fixing 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,6 +5,7 @@
 *@s1: A pointer to the first string to be compared.
 *@s2: A pointer to the second string to be compared.
 *Return: If str1 < str2, the negative difference of
+*
 *the first unmatched characters.
 *If s1 == s2, 0.
 *If s1 > s2, the positive difference of
@@ -16,7 +17,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		s1++;
 		s2++;
+	}
 
 		return (*s1 - *s2);
-	}
 }
