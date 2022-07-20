8d383e1cf1a353725326998404f289935e9ac3a2
Emwende <emwende89@gmail.com>
2022-07-20 14:47:49 -0700

    prints length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
-/*
-* _strlen_recursion - Returns the length of a string.
-*
-*@s: The string to be measured.
-
-*Return: The length of the string
+/**
+*_strlen_recursion -  function that returns the length of a string
+*@s: the pointer of char
+*Return: the value length
 */
 int _strlen_recursion(char *s)
 {
