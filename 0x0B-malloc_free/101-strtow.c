32bc40f434a8f50d64dff0bd08f878770f0b00c8
Emwende <emwende89@gmail.com>
2022-07-26 12:07:47 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -15,24 +15,6 @@ int word_len(char *str)
 {
 	int index = 0, len = 0;
 
-	while (*(str + index) && *(str + index) != ' ')
-	{
-		len++;
-		index++;
-	}
-	return (len);
-}
-
-/**
- * count_words - Counts the number of words contained within a string.
- * @str: The string to be searched.
- *
- * Return: The number of words contained within str.
- */
-int count_words(char *str)
-{
-	int index = 0, words = 0, len = 0;
-
 	for (index = 0; *(str + index); index++)
 		len++;
 
@@ -49,11 +31,10 @@ int count_words(char *str)
 }
 
 /**
- * strtow - Splits a string into words.
- * @str: The string to be split.
- *
- * Return: If str = NULL, str = "", or the function fails - NULL.
- *  Otherwise - a pointer to an array of strings (words).
+ * strtow - splits a string into words
+ * @str: the string to be split
+ *  Return: if str = NULL, str = "", or the function fails - NULL
+ *   otherwise - a pointer to an array of strings (words)
  */
 char **strtow(char *str)
 {
@@ -89,15 +70,29 @@ char **strtow(char *str)
 			return (NULL);
 		}
 
-			for (l = 0; l < letters; l++)
-				strings[w][l] = str[index++];
+		for (l = 0; l < letters; l++)
+			strings[w][l] = str[index++];
 
-			strings[w][l] = '\0';
+		strings[w][l] = '\0';
 	}
 	strings[w] = NULL;
 
 	return (strings);
 }
+int index = 0, words = 0, len = 0;
 
+	for (index = 0; *(str + index); index++)
+		len++;
 
+	for (index = 0; index < len; index++)
+	{
+		if (*(str + index) != ' ')
+		{
+			words++;
+			index += word_len(str + index);
+		}
+	}
+
+	return (words);
+}
 
