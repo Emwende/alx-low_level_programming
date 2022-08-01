936081b773b733ad3327cef6e2b0dad1f606cb9c
Emwende <emwende89@gmail.com>
2022-08-01 11:41:55 -0700

    a program that prints the name of the file it was compiled from, followed by a new line

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,13 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#include <stdio.h>
 
-#define ABS(x) ((x) < (0) ? -(x) : (x))
-
-#endif
+/**
+*main - prints the name of the file
+*
+*Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
